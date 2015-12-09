57ad96dada7c5a75425980afd725a394067ae1ff
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-12-09 09:30:34 +0800

    handling missing icons: using the default one from the system package manager

--- src/main/java/trikita/hut/ActionsAdapter.java
+++ src/main/java/trikita/hut/ActionsAdapter.java
@@ -1,6 +1,8 @@
 package trikita.hut;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -31,6 +33,9 @@ public class ActionsAdapter extends SimpleCursorAdapter {
 
     public ActionsAdapter(Context c, final ActionsProvider.Category category, final ViewBinder viewBinder) {
         super(c, R.layout.item, App.actions().query(category, ""), COLUMNS, VIEWS);
+        if (mMissingIcon == null) {
+            mMissingIcon = c.getPackageManager().getDefaultActivityIcon();
+        }
         setFilterQueryProvider(new FilterQueryProvider() {
             public Cursor runQuery(CharSequence constraint) {
                 return App.actions().query(category, constraint.toString());
@@ -47,36 +52,35 @@ public class ActionsAdapter extends SimpleCursorAdapter {
                         imageView.setImageDrawable(d);
                     } else {
                         imageView.setImageDrawable(null);
-                        if (uri != null) {
-                            imageView.post(new Runnable() {
-                                public void run() {
-                                    try {
-                                        Drawable drawable = null;
+                        imageView.post(new Runnable() {
+                            public void run() {
+                                try {
+                                    Drawable drawable = null;
+                                    if (uri != null) {
                                         if (uri.startsWith("data:image/png;base64,")) {
-                                            String b64 = uri.substring(uri.indexOf(",")+1);
+                                            String b64 = uri.substring(uri.indexOf(",") + 1);
                                             byte[] raw = Base64.decode(b64, 0);
                                             drawable = new BitmapDrawable(BitmapFactory.decodeByteArray(raw, 0, raw.length));
                                         } else {
                                             InputStream inputStream = view.getContext().getContentResolver().openInputStream(Uri.parse(uri));
                                             drawable = Drawable.createFromStream(inputStream, uri);
                                         }
-                                        if (drawable != null) {
-                                            mCache.put(uri, drawable);
-                                            notifyDataSetChanged();
-                                        }
-                                    } catch (FileNotFoundException e) {
                                     }
+                                    if (drawable != null) {
+                                        mCache.put(uri, drawable);
+                                    } else {
+                                        mCache.put(uri, mMissingIcon);
+                                    }
+                                    notifyDataSetChanged();
+                                } catch (FileNotFoundException e) {
+                                    mCache.put(uri, mMissingIcon);
                                 }
-                            });
-                        }
+                            }
+                        });
                     }
                     return true;
                 }
-                if (viewBinder != null) {
-                    return viewBinder.setViewValue(view, cursor, columnIndex);
-                } else {
-                    return false;
-                }
+                return viewBinder != null && viewBinder.setViewValue(view, cursor, columnIndex);
             }
         });
     }
